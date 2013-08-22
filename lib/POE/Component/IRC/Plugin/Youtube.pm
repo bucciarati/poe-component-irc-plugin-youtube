a7928b5d88eea0dc357c65505a79bd526c2f8411
Fernando Vezzosi <buccia@repnz.net>
2013-08-22 21:27:57 +0000

    Initial commit, it's been working for a while

---
+++ lib/POE/Component/IRC/Plugin/Youtube.pm
@@ -0,0 +1,101 @@
+package POE::Component::IRC::Plugin::Youtube;
+
+use strict;
+use warnings;
+
+use POE::Component::IRC::Plugin::Youtube::Tools;
+
+use POE::Component::IRC;
+use POE::Component::IRC::Plugin qw( :ALL );
+
+# needed to avoid: Net::SSL from Crypt-SSLeay cant verify hostnames; either install IO::Socket::SSL or turn off verification by setting the PERL_LWP_SSL_VERIFY_HOSTNAME environment variable to 0 at /usr/share/perl5/LWP/Protocol/http.pm line 51.
+use IO::Socket::SSL;
+
+use HTTP::Request;
+use XML::RSS;
+
+sub new {
+    my ($package, %args) = @_;
+
+    my $self = bless \%args, $package;
+
+    $self->{rss} = XML::RSS->new();
+
+    return $self;
+}
+
+sub PCI_register {
+    my ($self, $irc) = @_;
+
+    $irc->plugin_register($self, 'SERVER', 'ping');
+    $irc->plugin_register($self, 'SERVER', 'botcmd_channels');
+    $irc->plugin_register($self, 'SERVER', 'botcmd_check');
+
+    my $botcmd;
+    foreach my $plugin ( values %{ $irc->plugin_list } ){
+        if ( $plugin->isa('POE::Component::IRC::Plugin::BotCommand') ){
+            $botcmd = $plugin;
+            last;
+        }
+    }
+    die __PACKAGE__ . " depends on BotCommand plugin\n" unless defined $botcmd;
+
+    $botcmd->add(channels => 'usage: channels');
+    $botcmd->add(check    => 'usage: check');
+
+    return 1;
+}
+
+sub PCI_unregister {
+    return 1;
+}
+
+sub S_botcmd_channels {
+    my ($self, $irc) = (shift, shift);
+
+    my $nick = shift;
+    my $channel = shift;
+    my $message = shift;
+
+    $irc->yield(
+        notice => $$channel,
+        "Following: [@{[ join ', ', @{ $self->{channels} } ]}]",
+    );
+
+    return PCI_EAT_NONE;
+}
+
+sub S_botcmd_check {
+    my ($self, $irc) = (shift, shift);
+
+    my $nick = shift;
+    my $channel = shift;
+    my $message = shift;
+
+    POE::Component::IRC::Plugin::Youtube::Tools::periodic_check( $irc, $self->{channels} );
+
+    return PCI_EAT_NONE;
+}
+
+sub S_ping {
+    my ($self, $irc) = (shift, shift);
+
+    $self->{last_check_time} //= time;
+    my $secs_past = time - $self->{last_check_time};
+
+    if ($secs_past >= $self->{minutes_between_checks} * 60){
+        warn "re-checking youtube feeds\n" if $self->{debug};
+
+        POE::Component::IRC::Plugin::Youtube::Tools::periodic_check( $irc, $self->{channels} );
+
+        $self->{last_check_time} = time;
+    }
+
+    return PCI_EAT_NONE;
+}
+
+1;
+
+__END__
+
+# vim: tabstop=4 shiftwidth=4 expandtab cindent:
---
+++ lib/POE/Component/IRC/Plugin/Youtube/Tools.pm
@@ -0,0 +1,83 @@
+package POE::Component::IRC::Plugin::Youtube::Tools;
+
+use strict;
+use warnings;
+
+use v5.014;
+
+use Data::Dumper;
+
+use DateTime::Format::Strptime;
+use LWP::UserAgent;
+use XML::RSS;
+
+my $ua = LWP::UserAgent->new;
+$ua->timeout(10);
+
+my $rss = XML::RSS->new();
+
+my $dt_parser = DateTime::Format::Strptime->new(
+    pattern => '%a, %d %b %Y %T +0000',
+    locale => 'C',
+    time_zone => 'Europe/Vatican',
+);
+
+my $ts_emitter = DateTime::Format::Strptime->new(
+    pattern => '%s',
+);
+
+my $status_file = $ENV{HOME} . '/.pocoirc-youtube-status';
+my $startup_timestamp = time;
+
+sub periodic_check {
+    my ($irc, $usernames) = @_;
+
+    my $last_seen_by_username = ( do $status_file ) // {};
+
+    foreach my $username ( @$usernames ){
+        my $url = "https://gdata.youtube.com/feeds/base/users/$username/uploads?alt=rss&v=2&orderby=published";
+        my $response = $ua->get( $url );
+
+        unless ( $response->is_success ) {
+            warn "Request failed while re-checking youtube feeds [@{[ $response->status_line ]}]\n";
+            next;
+        }
+
+        my $feed_content = $response->decoded_content;
+        $rss->parse( $feed_content );
+
+        # so one doesn't get spammed with lots of entries when the bot starts up
+        $last_seen_by_username->{$username} //= $startup_timestamp;
+
+        for my $item ( @{ $rss->{items} } ) {
+            $item->{link} =~ s/&feature=youtube_gdata//;
+            $item->{timestamp} = $ts_emitter->format_datetime(
+                $dt_parser->parse_datetime( $item->{pubDate} )
+            );
+            warn "timestamp:<$item->{timestamp}> title:<$item->{title}> link:<$item->{link}>\n" if $self->{debug};
+
+            if ( $last_seen_by_username->{$username} < $item->{timestamp} ) {
+                $last_seen_by_username->{$username} = $item->{timestamp};
+
+                for my $chan_name ( keys $irc->{STATE}{Chans} ) {
+                    $irc->yield(
+                        notice => $chan_name,
+                        $item->{link} . ' ' . $item->{title},
+                    );
+                }
+            }
+        }
+
+        warn Dumper( $rss->{items}[0] ) if $self->{debug};
+    }
+
+    open my $fh, '>', $status_file;
+    print $fh Dumper( $last_seen_by_username );
+    $fh->close;
+}
+
+1;
+
+__END__
+
+# vim: tabstop=4 shiftwidth=4 expandtab cindent:
